7f75a698ee3a518bcb97f03f3548a1b4812be02d
mohanvelaga <mohanvelaga1998@gamil.com>
2026-04-19 18:34:57 +0530

    router fixed

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -1,11 +1,9 @@
-// src/routes/contact.routes.js
-
 const express = require('express')
 const router = express.Router()
 
 const { handleContact } = require('../controllers/contact.controller')
 const validate = require('../middleware/validate.middleware')
 
-app.use('/api/contact', require('./routes/contact.routes'))
+router.post('/', validate, handleContact)
 
 module.exports = router
